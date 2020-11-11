7f361d9d19e5a23b1485cbd48b5fc1a5a5094609
Anna Humeniuk <aznosenko@gmail.com>
2020-11-11 23:54:34 +0300

    последняя попытка

--- js/form.js
+++ js/form.js
@@ -1,7 +1,18 @@
 const findHotel = document.querySelector(".find-hotel");
 const findPopup = document.querySelector(".modal-form");
+const findHotelForm = findPopup.querySelector(".find-hotel-form");
+const arrivalDate = findPopup.querySelector(".form-arrival-date");
+const departureDate = findPopup.querySelector(".form-departure-date");
 
 findHotel.addEventListener("click", function (evt) {
   evt.preventDefault();
   findPopup.classList.toggle("form-show");
+  arrivalDate.focus();
+});
+
+findHotelForm.addEventListener("submit", function (evt) {
+  if (!arrivalDate.value || !departureDate.value) {
+    evt.preventDefault();
+    findPopup.classList.add("form-error");
+  }
 });
